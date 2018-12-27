d6b7626820b66e772fe9c13ab09d745f14f8fd87
kiran kumar <kirankdev07@gmail.com>
2018-12-26 21:33:49 -0500

    google sign in added

--- Google/Google/ViewController.swift
+++ Google/Google/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        let signInButton = GIDSignInButton(frame: CGRect (x: 0, y: 0, width: 100, height: 50))
+        signInButton.center = view.center
+        view.addSubview(signInButton)
+    }
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Initialize sign-in
+        var configureError: NSError?
+       
+        assert(configureError == nil, "Error configuring Google services: \(configureError)")
+        GIDSignIn.sharedInstance().clientID = "Cliend id From GoogleService-Info.plist file"
+        GIDSignIn.sharedInstance().delegate = self as! GIDSignInDelegate
+        return true
+        GIDSignIn.sharedInstance().clientID = "kirankumarb944@gmail.com"
     }
 
-
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        print(user.profile.email)
+    }
 }
 
