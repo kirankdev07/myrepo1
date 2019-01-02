8ac5728a4fab801a7847de1f0b73471618971c78
kirankdev07 <45579444+kirankdev07@users.noreply.github.com>
2019-01-02 11:38:27 -0500

    prime videos

---
+++ PrimeVideos/PrimeVideos/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PrimeVideos
+//
+//  Created by Kirankumar Bollem on 11/13/18.
+//  Copyright © 2018 Kirankumar Bollem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var homeBttn: UIButton!
+    
+    @IBOutlet weak var primeBttn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
